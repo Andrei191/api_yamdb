200255ff1796cfdaf54e25d073b012d5fcef2bb5
unknown <pavelsereda42@gmail.com>
2022-03-16 23:46:08 +0300

    added signup system

--- api_yamdb/auth/urls.py
+++ api_yamdb/auth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from auth.views import MyObtainTokenPairView
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from auth.views import RegisterView
 
 
 urlpatterns = [
-    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('signup/', RegisterView.as_view(), name='auth_register'),
 ]
--- api_yamdb/auth/views.py
+++ api_yamdb/auth/views.py
@@ -1,8 +1,19 @@
-from .serializers import MyTokenObtainPairSerializer
+from users.models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
 
 
-class MyObtainTokenPairView(TokenObtainPairView):
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
     permission_classes = (AllowAny,)
-    serializer_class = MyTokenObtainPairSerializer
+    serializer_class = RegisterSerializer
+
+    def post(self, serializer):
+        serializer = RegisterSerializer(data=serializer.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
