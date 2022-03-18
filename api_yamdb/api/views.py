fb4a467a1a83e0ffaa8cf097e51959e668caaa66
JaSlava <jakush9@bk.ru>
2022-03-18 08:18:47 +0300

    10SpDev2.1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from rest_framework import filters, permissions, viewsets
 
-from .models import Category, Comment, Genre, Review, Title
+from rest_framework.pagination import PageNumberPagination
+
+from reviews.models import Category, Comment, Genre, Review, Title
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer, TitleSerializer)
 
-#class TitleViewSet
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """View для модели Title."""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """View для модели Category."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """View для модели Genre."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
