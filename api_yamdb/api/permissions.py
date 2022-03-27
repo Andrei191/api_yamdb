9c400204f24b1e9743703c8742f32e9e2e366777
unknown <pavelsereda42@gmail.com>
2022-03-27 13:03:19 +0300

    last changes

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -26,7 +26,7 @@ class IsModerOrReadOnlyPermission(permissions.BasePermission):
                 or request.method in permissions.SAFE_METHODS)
 
     def has_object_permission(self, request, view, obj):
-        return request.role == "moderator"
+        return request.user.is_moderator
 
 
 class AuthorOrModerPermission(permissions.BasePermission):
@@ -41,7 +41,7 @@ class AuthorOrModerPermission(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-            or request.user.role == "moderator"
+            or request.user.is_moderator
             or request.user.is_staff
         )
 
